a75851e38a33976eb6045eea2e63c7e0250909ce
abdullah-taha5 <75340111+abdullah-taha5@users.noreply.github.com>
2024-06-25 17:11:03 +0300

    .

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const express = require('express')
 const app = express()
+const path = require('path'); 
 // const cors = require('cors')
 // app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
-  debug: true
+  debug: true,
+  path: '/peerjs'
 });
 const { v4: uuidV4 } = require('uuid')
 
 app.use('/peerjs', peerServer);
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
